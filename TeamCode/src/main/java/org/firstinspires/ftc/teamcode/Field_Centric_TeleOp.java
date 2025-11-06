ced4158cc1159a97c7a427b675f8493d8e16ffb3
Katie139 <kematz11@gmail.com>
2025-11-06 11:05:37 -0600

    Create Field_Centric_TeleOp.java
    added field centric teleop.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Field_Centric_TeleOp.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "FCDrivingITD", group = "LinearOpMode")
+public class Field_Centric_TeleOp {
+}
